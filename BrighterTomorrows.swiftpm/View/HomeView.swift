d4151ed8ba2d876b2d72de69872ecd07fb8eb149
Tri Lai <s3799602@rmit.edu.vn>
2024-02-18 23:20:52 +0700

    Add welcome view.

---
+++ BrighterTomorrows.swiftpm/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Trí Lai on 18/02/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
