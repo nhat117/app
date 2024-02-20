f6879d85767addbbc904744d5cd38452e61d6510
Tri Lai <s3799602@rmit.edu.vn>
2024-02-20 23:46:53 +0700

    Add custom back button behaviour for quiz view.

---
+++ BrighterTomorrows.swiftpm/Components/QuestionCard.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Trí Lai on 20/02/2024.
+//
+
+import SwiftUI
+
+struct QuestionCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionCard()
+}
