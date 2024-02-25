2fa807ee91d1f7481b5e54b5d9b447d40a63eba7
nhatbuiminh <nhat117@gmail.com>
2024-02-25 22:15:10 +0700

    Add header file

--- BrighterTomorrows.swiftpm/Model/ContactResourceModel.swift
+++ BrighterTomorrows.swiftpm/Model/ContactResourceModel.swift
@@ -1,3 +1,16 @@
+/*  Author: Bui Minh Nhat
+    Email: s3878174@rmit.edu.vn
+    Created  date: 12/2/2023
+    Last modified: 25/2/2023
+    Acknowledgement:
+        - The UI designs are inspired from:
+            “Children Learning App,” Dribbble,
+ https://dribbble.com/shots/7265955-Children-Learning-App/attachments/222641?mode=media
+    (accessed Feb. 24, 2023).
+        - "Apple Documentation", Apple ,https://developer.apple.com/documentation/swiftui/     (accessed Feb. 22, 2023).
+        - "Hacking With Swift", Hacking With Swift, https://www.hackingwithswift.com/
+     (accessed Feb. 22, 2023).
+*/
 import Foundation
 
 struct ContactResourceModel : Identifiable {
--- BrighterTomorrows.swiftpm/View/ContactAndResource.swift
+++ BrighterTomorrows.swiftpm/View/ContactAndResource.swift
@@ -1,7 +1,22 @@
+/*  Author: Bui Minh Nhat
+    Email: s3878174@rmit.edu.vn
+    Created  date: 12/2/2023
+    Last modified: 25/2/2023
+    Acknowledgement:
+        - The UI designs are inspired from:
+            “Children Learning App,” Dribbble,
+ https://dribbble.com/shots/7265955-Children-Learning-App/attachments/222641?mode=media
+    (accessed Feb. 24, 2023).
+        - "Apple Documentation", Apple ,https://developer.apple.com/documentation/swiftui/     (accessed Feb. 22, 2023).
+        - "Hacking With Swift", Hacking With Swift, https://www.hackingwithswift.com/
+     (accessed Feb. 22, 2023).
+*/
 import SwiftUI
 
 struct ContactAndResource: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    //Storage
     @AppStorage("isDarkMode") private var isDarkMode: Bool = Theme.darkMode
     
     private var contactData: ContactResourceData = ContactResourceData()
